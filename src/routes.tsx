fa235abe389a82a69792d68c5025123523ad4316
Benjamin Masor <benjaminmasor@gmail.com>
2022-08-03 03:56:58 +0900

    added: feature/authorization & navbar authorization

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,22 +2,58 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 import { Layout } from './core/components/Layout';
-import { Auth } from './auth';
+import { Login } from './auth/login';
+import { Register } from './auth/register';
 import { Post } from './post';
 import { User } from './admin/user';
 import { NotificationProvider } from './contexts/notificationContext';
 import { Notification } from './core/components/Notification';
 
+import { useGetProfileQuery } from './graphql/generated';
+
 function PostRoutes() {
-  return (
+  const { data } = useGetProfileQuery();
+
+  const mode =
+    data?.getProfile?.data === null
+      ? 'guest'
+      : data?.getProfile?.data?.role === 'user'
+      ? 'user'
+      : 'admin';
+
+  return mode === 'guest' ? (
     <BrowserRouter>
       <NotificationProvider>
-        <Layout>
+        <Layout mode='guest'>
           <Routes>
-            <Route path='/auth/*' element={<Auth />} />
+            <Route path='/' element={<App />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+          <Notification />
+        </Layout>
+      </NotificationProvider>
+    </BrowserRouter>
+  ) : mode === 'admin' ? (
+    <BrowserRouter>
+      <NotificationProvider>
+        <Layout mode='admin'>
+          <Routes>
             <Route path='/' element={<App />} />
             <Route path='/posts/*' element={<Post />} />
             <Route path='/users/*' element={<User />} />
+          </Routes>
+          <Notification />
+        </Layout>
+      </NotificationProvider>
+    </BrowserRouter>
+  ) : (
+    <BrowserRouter>
+      <NotificationProvider>
+        <Layout mode='user'>
+          <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/posts/*' element={<Post />} />
           </Routes>
           <Notification />
         </Layout>
